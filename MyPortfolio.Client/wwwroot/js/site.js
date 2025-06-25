7b0131c8ccb1c9710fe99f262f6496a352b2233c
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-06-26 01:40:14 +0500

    FEAT: Add Navbar

---
+++ MyPortfolio.Client/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿function toggleDarkMode(mode) {
+    if (mode === "dark") {
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode");
+    }
+}

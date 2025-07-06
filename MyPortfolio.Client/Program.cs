5fae475b3eb35cb029dee65eb197dff5934eb563
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-06 15:12:12 +0500

    CONFIG: Update Frontend Build

--- MyPortfolio.Client/Program.cs
+++ MyPortfolio.Client/Program.cs
@@ -10,11 +10,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient
-{
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-});
-
 builder.Services.AddScoped<LanguageService>();
 builder.Services.AddScoped<AuthMessageHandler>();
 builder.Services.AddAuthorizationCore();

060c0b1d4c486255282cdea51425cdec0f55180f
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-06 14:25:51 +0500

    CONFIG: Include static-client build folder for Render deploy

---
+++ MyPortfolio.Client/static-client/release/wwwroot/js/init-scripts.js
@@ -0,0 +1,28 @@
+﻿window.addEventListener('load', function () {
+    var loader = document.getElementById('loader');
+    var path = window.location.pathname.toLowerCase();
+
+    if (path === '/admin/dashboard' || path === '/admin/login') {
+        loader.style.display = 'none';
+    } else {
+        setTimeout(() => {
+            loader.style.display = 'none';
+        }, 1000);
+    }
+});
+
+window.hideLoader = () => {
+    const loader = document.getElementById('loader');
+    const app = document.getElementById('app');
+
+    if (loader) loader.style.display = 'none';
+    if (app) app.style.display = 'block';
+
+    document.body.classList.remove('loading');
+    document.body.classList.add('ready');
+};
+
+window.setLang = (code) => {
+    localStorage.setItem("lang", code);
+    location.reload();
+};

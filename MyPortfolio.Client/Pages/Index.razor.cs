7f53b924007c5280c2dfa04e4d6083f90a82eef6
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-08 18:12:43 +0500

    CODE RUB: Clean Up

--- MyPortfolio.Client/Pages/Index.razor.cs
+++ MyPortfolio.Client/Pages/Index.razor.cs
@@ -5,27 +5,6 @@ namespace MyPortfolio.Client.Pages
 {
     public partial class Index
     {
-        [Inject] public IJSRuntime JS { get; set; } = default!;
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (firstRender)
-            {
-                await JS.InvokeVoidAsync("observeAndStoreSection");
-                await JS.InvokeVoidAsync("setupScrollSnap");
-                await JS.InvokeVoidAsync("setupSectionAnimations");
-
-                var hash = await JS.InvokeAsync<string>("eval", "location.hash.substring(1)");
-                if (!string.IsNullOrEmpty(hash))
-                {
-                    await JS.InvokeVoidAsync("scrollToSectionById", hash);
-                }
-            }
-        }
-
-        private async Task ScrollToSection(string id)
-        {
-            await JS.InvokeVoidAsync("scrollToSectionById", id);
-        }
+ 
     }
 }
---
+++ MyPortfolio.Client/Pages/Shared/FooterSection.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MyPortfolio.Client.Services;
+
+namespace MyPortfolio.Client.Pages.Shared
+{
+    public partial class FooterSection : ComponentBase, IDisposable
+    {
+        [Inject] public IJSRuntime JS { get; set; } = default!;
+        [Inject] public LanguageService Lang { get; set; } = default!;
+
+        private bool _langReady = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var culture = await JS.InvokeAsync<string>("localStorage.getItem", "lang") ?? "en-US";
+            await Lang.LoadAsync(culture);
+            _langReady = true;
+            StateHasChanged();
+        }
+
+        protected override void OnInitialized()
+        {
+            Lang.OnLanguageChanged += HandleLanguageChanged;
+        }
+
+        private void HandleLanguageChanged()
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void Dispose()
+        {
+            Lang.OnLanguageChanged -= HandleLanguageChanged;
+        }
+    }
+}
